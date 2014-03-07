fb2f416dfbb66a9de7a914c88512fcb3c66b2390
abhinigam <anigam@linkedin.com>
2014-03-06 16:24:09 -0800

    Pushing the lock-free version of FutureAdapterListener

--- network/src/test/scala/com/linkedin/norbert/network/common/NorbertFutureSpec.scala
+++ network/src/test/scala/com/linkedin/norbert/network/common/NorbertFutureSpec.scala
@@ -21,6 +21,7 @@ import org.specs.SpecificationWithJUnit
 import org.specs.mock.Mockito
 import java.util.concurrent.{TimeoutException, ExecutionException, TimeUnit, Executor, LinkedBlockingQueue, Executors}
 import scala.Right
+import java.util.Random
 
 class CurrentThreadExecutor extends Executor {
     def execute(r:Runnable) = {
@@ -86,6 +87,57 @@ class NorbertFutureSpec extends SpecificationWithJUnit with Mockito with SampleM
       queue.size must be(1)
       val response: ResponseExceptionWrapper = queue.poll
       response.isException mustEqual true
+    }
+
+    //FutureAdapterListener make sure if we come from norbert thread callback we get the response
+    "norbert thread should be able to return the correct response" in {
+      val future = new FutureAdapterListener[Ping]
+      val queue = new LinkedBlockingQueue[ResponseExceptionWrapper]()
+
+      future.addListener(new Task(queue))
+      queue.size must be(0)
+      val msg = new Ping
+      future.apply(Right(msg))
+      future.isDone must beTrue
+      queue.size must be(1)
+      queue.poll mustEqual ResponseExceptionWrapper(null, msg, false)
+    }
+
+    "concurrent norbert thread and calling thread should be able to handle the response" in {
+      //multiple runs with different thread execution orders hopefully
+      for (i <- 0 to 15) {
+        val future = new FutureAdapterListener[Ping]
+        val queue = new LinkedBlockingQueue[ResponseExceptionWrapper]()
+        val msg = new Ping
+        queue.size must be(0)
+
+        val randomGen = new Random(12345)
+        val thread1 = new Thread(new Runnable{
+          def run {
+            val timeToSleep = 20 + randomGen.nextInt() % 20
+            Thread.sleep(timeToSleep)
+            val task = new Task(queue)
+            future.addListener(task)
+          }
+        })
+        thread1.start()
+        val thread2 = new Thread(new Runnable {
+          def run {
+            val timeToSleep = 20 + randomGen.nextInt() % 20
+            Thread.sleep(timeToSleep)
+            future.apply(Right(msg))
+          }
+        })
+        thread2.start()
+
+        //just to make sure the number of active threads is not very high
+        thread1.join()
+        thread2.join()
+
+        future.isDone must beTrue
+        queue.size() mustEqual 1
+        queue.poll mustEqual ResponseExceptionWrapper(null, msg, false)
+      }
     }
   }
 }
